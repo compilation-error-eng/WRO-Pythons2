7674876bf5994e419bb9a2ffa7f39949d50d0692
Ahmed-Elsayed <53234566+compilation-error-eng@users.noreply.github.com>
2022-09-16 16:53:33 +0200

    motor codes

--- motor.ino
+++ motor.ino
@@ -1,8 +1,12 @@
 void right(){
-    myservo.write(155);
+    myservo.write (155);
   digitalWrite(IN3 , HIGH); //IN1 -->HIGH,IN2-->LOW   backwards
   digitalWrite(IN4 , LOW);  //IN1 -->LOW , IN2 -->HIGH forward
   analogWrite(ENB ,255);
+}
+void stops(){
+  myservo.write(110);
+  analogWrite(ENB,0);
 }
 void left(){
     myservo.write(85);
@@ -17,14 +21,20 @@ void forward(){
   analogWrite(ENB ,255);
 }
 void left_wall(){
-  myservo.write(95);
+  myservo.write(105);
   digitalWrite(IN3 , HIGH); //IN1 -->HIGH,IN2-->LOW   backwards
   digitalWrite(IN4 , LOW);  //IN1 -->LOW , IN2 -->HIGH forward
   analogWrite(ENB ,255);
 }
 void right_wall(){
-  myservo.write(130);
+  myservo.write(125);
   digitalWrite(IN3 , HIGH); //IN1 -->HIGH,IN2-->LOW   backwards
   digitalWrite(IN4 , LOW);  //IN1 -->LOW , IN2 -->HIGH forward
+  analogWrite(ENB ,255);
+}
+void backwards(){
+  myservo.write(110);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
   analogWrite(ENB ,255);
 } 
